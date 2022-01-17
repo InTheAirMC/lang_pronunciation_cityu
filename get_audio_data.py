f2f3a7263c4932bc9f7e1c0e4a02c3d76afa3679
InTheAirMC <51772748+InTheAirMC@users.noreply.github.com>
2022-01-17 12:53:51 +0800

    Uploaded Files
    Uploaded Files

---
+++ get_audio_data.py
@@ -0,0 +1,129 @@
+# import asyncio
+# import time
+# async def foo(text):
+#     print(text)
+#     await asyncio.sleep(1)
+#     print(text)
+#
+# async def loop(text):
+#     for x in range(10):
+#         print(text)
+#         await asyncio.sleep(1)
+#
+# async def main():
+#     task1 = asyncio.create_task(loop("loop"))
+#     task2 = asyncio.create_task(foo("foo"))
+#     await task1
+#
+# asyncio.run(main())
+# import pyaudio
+# import wave
+# import time
+# import asyncio
+# import tkinter as tk
+# # import tkMessageBox
+# # class Audio:
+# #     def __init__(self):
+# #         self.audio = pyaudio.PyAudio()
+# #         self.frames = []
+# #         self.sound_file = wave.open('test2.wav','wb')
+# #         # self.stream = False
+# #
+# #     def start_audio(self):
+# #         def callback(audiodevice, audiomemoryview,a,b):
+# #             """This is called in the sound thread.
+# #             Note, that the frequency and such you request may not be what you get.
+# #             """
+# #             # print(type(audiomemoryview), len(audiomemoryview))
+# #             # print(audiodevice)
+# #             self.frames.append(bytes(audiomemoryview))
+# #         self.stop = 0
+# #         self.stream = self.audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True,frames_per_buffer=1024)
+# #         for x in range(100):
+# #             data = self.stream.read(1024)
+# #             self.frames.append(data)
+# #             # time.sleep(.1)
+# #             print("Hi")
+# #         self.stop_audio()
+# #         #
+# #
+# #         #
+# #
+# #     def stop_audio(self):
+# #         print("Done")
+# #         self.stop = 1
+# #         self.stream.stop_stream()
+# #         self.stream.close()
+# #         # self.stream.terminate()
+# #         sound_file = wave.open('test2.wav', 'wb')
+# #         sound_file.setnchannels(1)
+# #         sound_file.setsampwidth(self.audio.get_sample_size(pyaudio.paInt16))
+# #         sound_file.setframerate(44100)
+# #         sound_file.writeframes(b''.join(self.frames))
+# #         sound_file.close()
+
+
+import requests
+import pprint
+import json
+import http.client
+import base64
+
+def read_json(json_file):
+    with open(json_file,'r') as f:
+        data = json.load(f)
+        return data
+def dump_json(data,json_file):
+    # data = json.loads(data)
+    with open(json_file, 'w') as f:
+        json.dump(data,f)
+
+def get_audio_data(word):
+    enc = base64.b64encode(open("output.wav", "rb").read())
+
+
+    conn = http.client.HTTPSConnection("pronunciation-assessment1.p.rapidapi.com")
+
+    headers = {
+        'content-type': "application/json",
+        'x-rapidapi-host': "pronunciation-assessment1.p.rapidapi.com",
+        'x-rapidapi-key': "3bbcf35f2fmsh2bddf79ad79f41ep14f141jsn1c195dc62d92"
+        }
+    data = read_json('audio_detail.json')
+    data["audio_base64"] = enc.decode("ascii")
+    data["audio_format"] = "wav"
+    data["text"] = word
+    dump_json(data,"audio_detail.json")
+    payload = json.dumps(read_json('audio_detail.json'))
+
+
+    conn.request("POST", "/pronunciation", payload, headers)
+
+    res = conn.getresponse()
+    data = res.read()
+    data = json.loads(data.decode("utf-8"))
+
+    dump_json(data,'audio_pronun_detail.json')
+    array_text = {"array":[],"score":0,"accent":[]}
+
+    for y in range(len(data["words"])):
+        array_text["array"].append([])
+        array_text["array"][y].append(data["words"][y]["label"])
+        for x in data["words"][y]["syllables"]:
+            array_text["array"][y].append([x["label"],x["score"]])
+
+
+    array_text["score"] = data["score"]
+    max_accent = max([data["accent_predictions"]["en_US"],data["accent_predictions"]["en_UK"],data["accent_predictions"]["en_AU"]])
+    max_accent_name = ""
+    if max_accent == data["accent_predictions"]["en_US"]:
+        max_accent_name = "en_US"
+    elif max_accent == data["accent_predictions"]["en_UK"]:
+        max_accent_name = "en_UK"
+    elif max_accent == data["accent_predictions"]["en_AU"]:
+        max_accent_name = "en_AU"
+    array_text["accent"].append(max_accent)
+    array_text["accent"].append(max_accent_name)
+    return array_text
+
+print(get_audio_data("Happy Joe"))
